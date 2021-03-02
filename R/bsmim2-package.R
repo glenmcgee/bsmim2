4674c3394dc3529608f879c2bd56845638c79f84
Ander Wilson <anderwilson@Anders-MacBook-Pro.local>
2021-03-02 10:51:10 -0700

    add package help and doc file with useDynLib added.

---
+++ R/bsmim2-package.R
@@ -0,0 +1,10 @@
+#' Bayesian Single and Multiple Index Models
+#' 
+#' This package fits Bayesian single and multiple index models including variable selection.
+#' 
+#' @docType package
+#' @author ander wilson <ander.wilson@colostate.edu>
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib bsmim2
+#' @name bsmim2
+NULL 
